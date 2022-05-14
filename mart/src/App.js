12ad37fae5366757e445937219cc4543cf5712a8
nishanthan <nishanthan@codevus.com>
2022-05-14 21:40:23 +0530

    nishan 1st

--- mart/src/App.js
+++ mart/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import Product from './components/Product';
-import Home  from  './components/Home';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Product from "./components/Product";
+import Home from "./components/Home";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
 import Login from "./components/Login";
@@ -12,37 +12,25 @@ import Aboutus from "./components/Aboutus";
 
 import { Routes, Route } from "react-router-dom";
 
-
+//dang sapada
 function App() {
   return (
     <>
       <Navbar />
-     
-  <Routes>
-      <Route path="/"  element={<Home />}/>
-       <Route path="/products" element={<Products/>}/>
-       <Route path="/products/:id" element={<Product/>}/>
-         <Route path="/cart" element={<Cart/>}/>
-                <Route path="/login" element={<Login/>}/>
-                 <Route path="/contact" element={<Contactus/>}/>
-                  <Route path="/about" element={<Aboutus/>}/>
-                   <Route path="/register" element={<Signup/>}/>
-           
- 
-  
- 
-  
-</Routes>
 
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contactus />} />
+        <Route path="/about" element={<Aboutus />} />
+        <Route path="/register" element={<Signup />} />
+      </Routes>
 
       <></>
-     
-     </>
-
-
-     
-  
+    </>
   );
 }
 
